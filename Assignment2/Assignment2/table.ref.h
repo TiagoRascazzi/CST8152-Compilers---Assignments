ab47c864370c1dd8b21043d0a4bab1501731807c
almightycloud <almightycloud@gmail.com>
2018-02-25 15:49:22 -0500

    Added table Reference, cleaned up original.

--- Assignment2/Assignment2/table.ref.h
+++ Assignment2/Assignment2/table.ref.h
@@ -35,44 +35,49 @@
 */
 
 
-/*REPLACE *ESN* WITH YOUR ERROR STATE NUMBER*/
-#define ES 1 /*ESN* /* Error state */
+REPLACE *ESN* WITH YOUR ERROR STATE NUMBER
+#define ES  *ESN* /* Error state */
 #define IS -1    /* Inavalid state */
 
 /* State transition table definition */
 
-/*REPLACE *CN* WITH YOUR COLUMN NUMBER*/
+REPLACE *CN* WITH YOUR COLUMN NUMBER
 
-#define AS5 1 /*TODO REMOVE */
-#define AS2 1 /*TODO REMOVE*/
-#define AS8  /*TODO REMOVE*/
-
-#define TABLE_COLUMNS 1 /*CN*/
+#define TABLE_COLUMNS *CN*
 /*transition table - type of states defined in separate table */
 int  st_table[][TABLE_COLUMNS] = {
-
-	/* TODO Add working table from reference*/
-
-};
+	/*        [a-zA-Z]   ---   0   ---   [1-9]   ---   .   ---   $   ---   other */
+	/* 1 */{ 1,              6,         4,           ES,       ES,       ES },
+	/* 2 */{ 1,              1,         1,           ES / AS2, 3,        2 },
+	/* 3 */{ IS,             IS,        IS,          IS,       IS,       IS },
+	/* 4 */{ IS,             IS,        IS,          IS,       IS,       IS },
+	/* 5 */{ ES / AS5,       4,         4,           7,        5,        5 },
+	/* 6 */{ IS,             IS,        IS,          IS,       IS,       IS },
+	/* 7 */{ ,               ,          ,            7,        ES / AS5, 5 },
+	/* 8 */{ ES / AS8,       7,         7,           ES / AS8, 8,        8 },
+	/* 9 */{ IS,             IS,        IS,          IS,       IS,       IS },
+	/* 10 */{ ,               ,          ,            ,         , },
+	/* 11 */{ ,               ,          ,            ,         , },
+	/* 12 */{ IS,             IS,        IS,          IS,       IS,       IS },
+	/* 13 */{ IS,             IS,        IS,          IS,       IS,       IS } };
 
 /* Accepting state table definition */
 /*REPLACE *N1*, *N2*, and *N3* WITH YOUR NUMBERS */
-#define ASWR  0   /*N1*  /* accepting state with retract */
-#define ASNR  0  /*N2*  /* accepting state with no retract */
-#define NOAS  0  /*N3*  /* not accepting state */
+#define ASWR     *N1*  /* accepting state with retract */
+#define ASNR     *N2*  /* accepting state with no retract */
+#define NOAS     *N3*  /* not accepting state */
 
-int as_table[] = { /*YOUR INITIALIZATION HERE - USE ASWR, ASNR, NOAS*/ };
+int as_table[] = { YOUR INITIALIZATION HERE - USE ASWR, ASNR, NOAS };
 
 /* Accepting action function declarations */
 
-/*FOR EACH OF YOUR ACCEPTING STATES YOU MUST PROVIDE
+FOR EACH OF YOUR ACCEPTING STATES YOU MUST PROVIDE
 ONE FUNCTION PROTOTYPE.THEY ALL RETURN Token AND TAKE
 ONE ARGUMENT : A string REPRESENTING A TOKEN LEXEME.
-*/
 
 Token aa_funcXX(char *lexeme);
 
-/*Replace XX with the number of the accepting state : 02, 03 and so on. */
+Replace XX with the number of the accepting state : 02, 03 and so on.
 
 /* defining a new type: pointer to function (of one char * argument)
 returning Token
@@ -89,12 +94,11 @@ typedef Token(*PTR_AAF)(char *lexeme);
 PTR_AAF aa_table[] = {
 
 
-	/*HERE YOU MUST PROVIDE AN INITIALIZATION FOR AN ARRAY OF POINTERS
+	HERE YOU MUST PROVIDE AN INITIALIZATION FOR AN ARRAY OF POINTERS
 	TO ACCEPTING FUNCTIONS.THE ARRAY HAS THE SAME SIZE AS as_table[].
 	YOU MUST INITIALIZE THE ARRAY ELEMENTS WITH THE CORRESPONDING
 	ACCEPTING FUNCTIONS(FOR THE STATES MARKED AS ACCEPTING IN as_table[]).
 	THE REST OF THE ELEMENTS MUST BE SET TO NULL.
-	*/
 
 };
 

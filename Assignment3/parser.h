19cc734a92ca11ba47ec3792bb296ab6a40e8679
Donc0003 <donc0003@algonquinlive.com>
2018-04-18 17:22:19 -0400

    Started working on parser

--- Assignment3/parser.h
+++ Assignment3/parser.h
@@ -1,1 +1,62 @@
-#pragma once
+#ifndef  PARSER_H
+#define  PARSER_H
+
+/* Token Attributes */
+#define NO_ATTR -1 
+#define ELSE 0
+#define FALSE 1
+#define IF 2
+#define PLATYPUS 3
+#define READ 4
+#define REPEAT 5
+#define THEN 6
+#define TRUE 7
+#define WHILE 8
+#define WRITE 9
+
+void parser(Buffer * in_buf);
+void match(int pr_token_code, int pr_token_attribute);
+void syn_eh(int sync_token_code);
+void syn_printe();
+void gen_incode(char* str);
+
+/*void program(void);
+void statements(void);
+void statements_prime(void);
+void opt_statements(void);
+void statement(void);
+void assignment_statement(void);
+void assignment_expression(void);
+void selection_statement(void);
+void iteration_statement(void);
+void pre_condition(void);
+void input_statement(void);
+void variable_list(void);
+void variable_list_prime(void);
+void opt_variable_list(void);
+void variable_identifier(void);
+void output_statement(void);
+void output_statement_argument(void);
+void arithmetic_expression(void);
+void unary_arithmetic_expression(void);
+void additive_arithmetic_expression(void);
+void additive_arithmetic_expression_prime(void);
+void multiplicative_arithmetic_expression(void);
+void multiplicative_arithmetic_expression_prime(void);
+void primary_arithmetic_expression(void);
+void string_expression(void);
+void string_expression_prime(void);
+void primary_string_expression(void);
+void conditional_expression(void);
+void logical_OR_expression(void);
+void logical_OR_expression_prime(void);
+void logical_AND_expression(void);
+void logical_AND_expression_prime(void);
+void relational_expression(void);
+void arithmetic_relational_expression(void);
+void string_relational_expression(void);
+void primary_a_relational_expression(void);
+void primary_s_relational_expression(void);
+*/
+
+#endif
